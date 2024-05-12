a8d1a254b0d87506fe8f20127cdd3e804df13fa4
Hprairie <hprairie17@gmail.com>
2024-05-12 16:27:24 -0500

    fixed treesitter bug

--- .config/nvim/lua/prairie/plugins/telescope.lua
+++ .config/nvim/lua/prairie/plugins/telescope.lua
@@ -9,10 +9,10 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 			vim.keymap.set("n", "<leader>fv", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fm", builtin.man_pages, {})
-            vim.keymap.set("n", "<leader>fh", builtin.current_buffer_fuzzy_find, {})
-            vim.keymap.set("n", "<leader>fi", builtin.help_tags, {})
-            vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
+			vim.keymap.set("n", "<leader>fm", builtin.man_pages, {})
+			vim.keymap.set("n", "<leader>fh", builtin.current_buffer_fuzzy_find, {})
+			vim.keymap.set("n", "<leader>fi", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
 		end,
 	},
 }
--- .config/nvim/snippets/tex/math-functions.lua
+++ .config/nvim/snippets/tex/math-functions.lua
@@ -42,6 +42,24 @@ local auto_backslash_snippet = function(context, opts)
 	)
 end
 
+local symbol_snippet = function(context, command, opts)
+	opts = opts or {}
+	if not context.trig then
+		error("context doesn't include a `trig` key which is mandatory", 2)
+	end
+	context.dscr = context.dscr or command
+	context.name = context.name or command:gsub([[\]], "")
+	context.docstring = context.docstring or (command .. [[{0}]])
+	context.wordTrig = context.wordTrig or false
+	j, _ = string.find(command, context.trig)
+	if j == 2 then -- command always starts with backslash
+		context.trigEngine = "ecma"
+		context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
+		context.hidden = true
+	end
+	return autosnippet(context, t(command), opts)
+end
+
 SNIPPET_MATH = {
 	-- Blocks
 	s(
@@ -96,7 +114,18 @@ SNIPPET_MATH = {
 		fmta([[\{<>\}<>]], {
 			c(1, { r(1, ""), sn(nil, { r(1, ""), t(" \\mid "), i(2) }), sn(nil, { r(1, ""), t(" \\colon "), i(2) }) }),
 			i(0),
-		})
+		}),
+		{ condition = is_math, show_condition = is_math }
+	),
+	s(
+		{ trig = "nnn", name = "bigcap", dscr = "bigcap", snippetType = "autosnippet" }, -- overload with set builders notation because analysis and algebra cannot agree on a singular notation
+		fmta([[\bigcap<> <>]], { c(1, { fmta([[_{<>}^{<>}]], { i(1, "i = 0"), i(2, "\\infty") }), t("") }), i(0) }),
+		{ condition = is_math, show_condition = is_math }
+	),
+	s(
+		{ trig = "uuu", name = "bigcup", dscr = "bigcup", snippetType = "autosnippet" }, -- overload with set builders notation because analysis and algebra cannot agree on a singular notation
+		fmta([[\bigcup<> <>]], { c(1, { fmta([[_{<>}^{<>}]], { i(1, "i = 0"), i(2, "\\infty") }), t("") }), i(0) }),
+		{ condition = is_math, show_condition = is_math }
 	),
 	s(
 		{ trig = "bnc", name = "binomial", dscr = "binomial (nCR)", snippetType = "autosnippet" },
@@ -119,9 +148,7 @@ SNIPPET_MATH = {
 		fmta([[\frac{\partial <>}{\partial <>}<>]], { i(1), i(2), i(0) }),
 		{ condition = is_math, show_condition = is_math }
 	),
-	-- Symbols with auto backslash
-
-	-- Additional Symbols
+	-- Additional Functions
 	s(
 		{ trig = "sq", describe = "Creates a sqrt function", snippetType = "autosnippet" },
 		fmta([[\sqrt<><>]], { c(1, { fmta([[{<>}]], { i(1, "") }), fmta([[[<>]{<>}]], { i(1, "2"), i(2) }) }), i(0) }),
@@ -154,7 +181,6 @@ local auto_backslash_specs = {
 	"argmin",
 	"deg",
 	"angle",
-	"cdot",
 	"quad",
 	"to",
 	--"in",
@@ -165,5 +191,79 @@ for _, v in ipairs(auto_backslash_specs) do
 	table.insert(auto_backslash_snippets, auto_backslash_snippet({ trig = v }, { condition = is_math }))
 end
 vim.list_extend(SNIPPET_MATH, auto_backslash_snippets)
+
+local symbol_specs = {
+	-- operators
+	["!="] = { context = { name = "!=" }, command = [[\neq]] },
+	["<="] = { context = { name = "≤" }, command = [[\leq]] },
+	[">="] = { context = { name = "≥" }, command = [[\geq]] },
+	["<<"] = { context = { name = "<<" }, command = [[\ll]] },
+	[">>"] = { context = { name = ">>" }, command = [[\gg]] },
+	["~~"] = { context = { name = "~" }, command = [[\sim]] },
+	["~="] = { context = { name = "≈" }, command = [[\approx]] },
+	["~-"] = { context = { name = "≃" }, command = [[\simeq]] },
+	["-~"] = { context = { name = "⋍" }, command = [[\backsimeq]] },
+	["-="] = { context = { name = "≡" }, command = [[\equiv]] },
+	["=~"] = { context = { name = "≅" }, command = [[\cong]] },
+	[":="] = { context = { name = "≔" }, command = [[\definedas]] },
+	["**"] = { context = { name = "·", priority = 100 }, command = [[\cdot]] },
+	xx = { context = { name = "×" }, command = [[\times]] },
+	["!+"] = { context = { name = "⊕" }, command = [[\oplus]] },
+	["!*"] = { context = { name = "⊗" }, command = [[\otimes]] },
+	-- sets
+	NN = { context = { name = "ℕ" }, command = [[\mathbb{N}]] },
+	ZZ = { context = { name = "ℤ" }, command = [[\mathbb{Z}]] },
+	QQ = { context = { name = "ℚ" }, command = [[\mathbb{Q}]] },
+	RR = { context = { name = "ℝ" }, command = [[\mathbb{R}]] },
+	CC = { context = { name = "ℂ" }, command = [[\mathbb{C}]] },
+	OO = { context = { name = "∅" }, command = [[\emptyset]] },
+	pwr = { context = { name = "P" }, command = [[\powerset]] },
+	cc = { context = { name = "⊂" }, command = [[\subset]] },
+	cq = { context = { name = "⊆" }, command = [[\subseteq]] },
+	qq = { context = { name = "⊃" }, command = [[\supset]] },
+	qc = { context = { name = "⊇" }, command = [[\supseteq]] },
+	["\\\\\\"] = { context = { name = "⧵" }, command = [[\setminus]] },
+	Nn = { context = { name = "∩" }, command = [[\cap]] },
+	UU = { context = { name = "∪" }, command = [[\cup]] },
+	["::"] = { context = { name = ":" }, command = [[\colon]] },
+	-- quantifiers and logic stuffs
+	AA = { context = { name = "∀" }, command = [[\forall]] },
+	EE = { context = { name = "∃" }, command = [[\exists]] },
+	inn = { context = { name = "∈" }, command = [[\in]] },
+	notin = { context = { name = "∉" }, command = [[\not\in]] },
+	["!-"] = { context = { name = "¬" }, command = [[\lnot]] },
+	VV = { context = { name = "∨" }, command = [[\lor]] },
+	WW = { context = { name = "∧" }, command = [[\land]] },
+	["!W"] = { context = { name = "∧" }, command = [[\bigwedge]] },
+	["=>"] = { context = { name = "⇒" }, command = [[\implies]] },
+	["=<"] = { context = { name = "⇐" }, command = [[\impliedby]] },
+	iff = { context = { name = "⟺" }, command = [[\iff]] },
+	["->"] = { context = { name = "→", priority = 250 }, command = [[\to]] },
+	["!>"] = { context = { name = "↦" }, command = [[\mapsto]] },
+	["<-"] = { context = { name = "↦", priority = 250 }, command = [[\gets]] },
+	-- differentials
+	dp = { context = { name = "⇐" }, command = [[\partial]] },
+	-- arrows
+	["-->"] = { context = { name = "⟶", priority = 500 }, command = [[\longrightarrow]] },
+	["<->"] = { context = { name = "↔", priority = 500 }, command = [[\leftrightarrow]] },
+	["2>"] = { context = { name = "⇉", priority = 400 }, command = [[\rightrightarrows]] },
+	upar = { context = { name = "↑" }, command = [[\uparrow]] },
+	dnar = { context = { name = "↓" }, command = [[\downarrow]] },
+	-- etc
+	ooo = { context = { name = "∞" }, command = [[\infty]] },
+	lll = { context = { name = "ℓ" }, command = [[\ell]] },
+	dag = { context = { name = "†" }, command = [[\dagger]] },
+	["+-"] = { context = { name = "†" }, command = [[\pm]] },
+	["-+"] = { context = { name = "†" }, command = [[\mp]] },
+}
+
+local symbol_snippets = {}
+for k, v in pairs(symbol_specs) do
+	table.insert(
+		symbol_snippets,
+		symbol_snippet(vim.tbl_deep_extend("keep", { trig = k }, v.context), v.command, { condition = is_math })
+	)
+end
+vim.list_extend(SNIPPET_MATH, symbol_snippets)
 
 return SNIPPET_MATH
